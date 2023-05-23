34740cf1b22313c44456ed3fff2f743415833890
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-05-24 01:06:30 +0600

    hero section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
-import Layout from "./Components/Layout";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Pages/Home";
+
 
 function App() {
   return <Layout/>
+  
 }
 
 export default App;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import '../styles/home.css'
+import {Container, Row,Col} from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../Shared/Subtitle';
+import SearchBar from '../Shared/SearchBar';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+
+    {/*Hero section start */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className='hero__content'>
+              <div className='hero__subtitle d-flex align-items-center'>
+                <Subtitle subtitle={'Know Before You Go'}/>
+                <img src={worldImg} alt='' />
+              </div>
+              <h1>
+                Travels opens  the door to creating{""}
+                <span className='highlight'> memorise</span>
+              </h1>
+
+              <p>
+              There are many variations of passages of Lorem Ipsum available, 
+              but the majority have suffered alteration in some form, 
+              by injected humour, suffered alteration in some form, 
+              by injected humour, by injected humour, by injected 
+              </p>
+            </div>
+          </Col>
+
+          <Col lg='2'>
+            <div className='hero__img-box'>
+              <img src={heroImg} alt='' />
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className='hero__img-box mt-4'>
+              <video src={heroVideo} alt='' controls/>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className='hero__img-box mt-5'>
+              <img src={heroImg02} alt='' />
+            </div>
+          </Col>
+
+          <SearchBar/>
+        </Row>
+      </Container>
+    </section>
+    
+    </>
   )
 }
 
