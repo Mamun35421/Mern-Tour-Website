2019d950fde27068099ceafe8e330225136ed1dd
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-06-07 02:49:55 +0600

    tour add

--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
+import CommonSection from '../Shared/CommonSection'
+import '../styles/tour.css'
+import tourData from '../assets/data/tours'
+import SearchBar from '../Shared/SearchBar'
+import TourCard from '../Shared/TourCard'
+import { Col, Container, Row } from 'reactstrap'
 
 const Tours = () => {
   return (
-    <div>Tour</div>
+    <>
+    <CommonSection title={"All Tours"} />
+
+    <section>
+      <Container>
+        <Row>
+          <SearchBar />
+        </Row>
+      </Container>
+    </section>
+
+    <section className='pt-0'>
+      <Container>
+        <Row>
+          {tourData?.map(tour =>(
+            <Col lg="3" className='mb-4' key={tour.id}>
+              <TourCard tour={tour} />
+            </Col>
+          ))}
+
+          <Col lg="12"></Col>
+        </Row>
+      </Container>
+    </section>
+    
+    </>
   )
 }
 
